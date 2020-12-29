97c57ac8dd5c21d7adf353b1c22d6b00e5bf9ac6
Lars Rademacher <rademacher.lars@gmx.de>
2020-12-29 18:02:31 +0100

    Basically working with glade

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,34 @@
 #include <gtk/gtk.h>
 
+GtkBuilder *builder;
+GtkWidget *window;
+
+int
+main(int argc, char **argv) {
+    gtk_init(&argc, &argv);
+
+    // build GUI from glade file
+    builder = gtk_builder_new_from_file("PdfTag_GtkGui.glade");    
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show(window);
+
+    gtk_main();
+
+    return EXIT_SUCCESS;
+
+    /*app = gtk_application_new("org.gtk.example",
+        G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate",
+        G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    return (status);*/
+}
+
 static void
 activate(GtkApplication *app,
     gpointer user_data) {
@@ -10,16 +39,11 @@ activate(GtkApplication *app,
     gtk_widget_show_all(window);
 }
 
-int
-main(int argc, char **argv) {
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new("org.gtk.example",
-        G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate",
-        G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
-    return (status);
+static void
+on_FileMenu_activate(GtkMenuItem *menuItem)
+{
+    if(gtk_menu_item_get_label(menuItem) == "Quit")
+    {
+        gtk_main_quit();
+    }
 }
